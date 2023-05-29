3fc79f372bfcb924e27a0178ff88b69e6e9bfa6c
E-E-Ewoma-Zino <eewoma75@gmail.com>
2023-05-29 18:27:15 +0100

    update/fex table autosizing

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,50 +9,105 @@ const serminalAlalysis = document.getElementById("serminalAlalysis");
 const chemicalPathology = document.getElementById("chemicalPathology");
 
 function notShow() {
-    printable.forEach(p => {
-        p.classList.remove("d-flex");
-        p.classList.add("d-none");
-    });
+	printable.forEach(p => {
+		p.classList.remove("d-flex");
+		p.classList.add("d-none");
+	});
 }
 
 function show() {
-    printable.forEach(p => {
-        p.classList.remove("d-none");
-        p.classList.add("d-flex");
-    });
+	printable.forEach(p => {
+		p.classList.remove("d-none");
+		p.classList.add("d-flex");
+	});
 }
 
 const printBtn = document.getElementById("printBtn");
 
 printBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    document.querySelectorAll(".mui-textfield>label").forEach(tf => tf.style.display = "none");
-    document.querySelectorAll("td>.mui-textfield>input").forEach(tf => tf.style.border = "none");
+	notShow();
 
-    notShow();
+	printable.forEach(pp => {
+		pp.querySelectorAll("td>.mui-textfield>label").forEach(tf => tf.style.display = "none");
+		pp.querySelectorAll("td>.mui-textfield>input").forEach(tf => tf.style.border = "none");
 
-    printable.forEach(pp => {
-        pp.querySelectorAll("input").forEach(inP => {
-            if (inP.type === "checkbox") {
-                if (inP.checked) {
-                    pp.classList.remove("d-none");
-                    pp.classList.add("d-flex");
-                }
-                return;
-            }
-            if (inP.value !== '') {
-                pp.classList.remove("d-none");
-                pp.classList.add("d-flex");
-            }
-        });
-    });
+		pp.querySelectorAll("input").forEach(inP => {
+			if (inP.type === "checkbox") {
+				if (inP.checked) {
+					pp.classList.remove("d-none");
+					pp.classList.add("d-flex");
+				}
+				return;
+			}
+			if (inP.value !== '') {
+				if (inP.parentElement.parentElement.tagName === "TD") {
+					try {
+						inP.parentElement.parentElement.parentElement.firstElementChild.firstElementChild.firstElementChild.checked = true;
+					} catch (error) {
+						console.error("Could not find a checkbox!");
+					}
 
-    window.print();
+					const value = inP.value;
+					const td = inP.parentElement.parentElement;
+
+					inP.parentElement.remove();
+					td.innerHTML = value;
+				}
+
+				// const isTrue = inP.parentElement.parentElement.parentElement.parentElement.classList.contains("printAble") || inP.parentElement.parentElement.parentElement.parentElement.parentElement.classList.contains("printAble") || inP.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.contains("printAble");
+				else {
+					const value = inP.value;
+					const label = inP.parentElement.lastElementChild.innerHTML;
+					const strong = document.createElement("strong");
+					const span = document.createElement("span");
+					const td = inP.parentElement.parentElement;
+
+					td.classList.add("fontLarge");
+
+					inP.parentElement.remove();
+					strong.innerHTML = label.toUpperCase();
+					span.innerHTML = ": " + value;
+					td.append(strong);
+					td.append(span);
+				}
+
+				pp.classList.remove("d-none");
+				pp.classList.add("d-flex");
+			}
+		});
+		pp.querySelectorAll("textarea").forEach(inP => {
+			if (inP.value !== '') {
+				const value = inP.value;
+				const label = inP.parentElement.lastElementChild.innerHTML;
+				const strong = document.createElement("strong");
+				const span = document.createElement("span");
+				const ta = inP.parentElement.parentElement;
+
+				ta.classList.add("fontLarge");
+				ta.classList.add("border-4");
+				ta.classList.add("border-bottom");
+
+				inP.parentElement.remove();
+				strong.innerHTML = label.toUpperCase();
+				span.innerHTML = ": " + value;
+				ta.append(strong);
+				ta.append(span);
+
+				pp.classList.remove("d-none");
+				pp.classList.add("d-flex");
+			}
+		});
+	});
+
+	window.print();
 });
 
-window.onafterprint = function () {    
-    document.querySelectorAll(".mui-textfield>label").forEach(tf => tf.style.display = "block");
-    document.querySelectorAll("td>.mui-textfield>input").forEach(tf => tf.style.border = "block");
-    show();
+window.onafterprint = function () {
+	document.querySelectorAll(".mui-textfield>label").forEach(tf => tf.style.display = "block");
+	document.querySelectorAll("td>.mui-textfield>input").forEach(tf => tf.style.border = "block");
+
+	show();
+	location.reload();
 }
